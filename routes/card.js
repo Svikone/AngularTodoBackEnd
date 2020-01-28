e20cae2ac740ccbe52046f2e67d3ab39b7841687
svikone <velichkovlad825@gmail.com>
2020-01-29 00:26:37 +0200

    fix rroute delete

--- routes/card.js
+++ routes/card.js
@@ -4,6 +4,6 @@ const Router = express.Router()
 
 Router.post("/add",  Controller.addCard);
 Router.post("/all",  Controller.allCards);
-Router.DELETE("/remove",  Controller.removeCards);
+Router.delete("/remove",  Controller.removeCards);
 
 module.exports = Router;
