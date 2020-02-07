af97129f536323b2e2bf3170b4a548ad77f6937e
svikone <velichkovlad825@gmail.com>
2020-02-08 00:20:49 +0200

    aa

--- controller/card.js
+++ controller/card.js
@@ -60,8 +60,8 @@ exports.addCardShared = (req, res) => {
         
 
         result[0]._idShared.push(req.body._idShared)
-        console.log(result)
-        Modules.updateOne(result).then(result => {
+        console.log(result[0])
+        Modules.updateOne(result[0]).then(result => {
             res.send(result).sendStatus(200);
         }).catch(err => {
             res.sendStatus(500)
