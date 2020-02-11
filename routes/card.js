689b7423bde1382f68eb1338926731e424ddf077
svikone <velichkovlad825@gmail.com>
2020-02-11 02:59:06 +0200

    qq

--- routes/card.js
+++ routes/card.js
@@ -9,8 +9,10 @@ Router.get("/all", mw, Controller.allCards);
 Router.delete("/remove/:id", mw,  Controller.removeCards);
 Router.get("/:id", mw,  Controller.getCardId);
 Router.post("/edit/:id", mw,  Controller.editCards);
-Router.post("/shared", mw,  Controller.addCardShared);
-Router.get("/shared", mw,  Controller.allCardShared);
+Router.post("/add/shared", mw,  Controller.addCardShared);
+Router.get("/all/shared", mw,  Controller.allCardShared);
+
+
 
 
 module.exports = Router;
